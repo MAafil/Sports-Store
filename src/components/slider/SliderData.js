19180ec19b3edab86b02ee6ef8f9c9f78005c6c9
MAafil <mahammadaafil@gmail.com>
2024-04-03 18:04:19 -0400

    Commiting Sports Store

---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,22 @@
+export const sliderData = [
+  {
+    image: "https://firebasestorage.googleapis.com/v0/b/javascript4-a0773.appspot.com/o/images%2F1b.jpg?alt=media&token=572f106b-12ac-452b-838f-93b40573618f",
+    heading: "Shoes Villa",
+    desc: "On sale up to 50% off",
+  },
+  {
+    image: "https://firebasestorage.googleapis.com/v0/b/javascript4-a0773.appspot.com/o/images%2F1a.jpg?alt=media&token=5481b586-7f47-4f00-ae73-a4c3d82be14f",
+    heading: "Kids Fashion",
+    desc: "On sale up to 50% off",
+  },
+  {
+    image: "https://firebasestorage.googleapis.com/v0/b/javascript4-a0773.appspot.com/o/images%2F1c.jpg?alt=media&token=8b14e76f-598c-444f-960e-37c4e83aaab4",
+    heading: "Men Fashion",
+    desc: "On sale up to 50% off",
+  },
+  {
+    image: "https://firebasestorage.googleapis.com/v0/b/javascript4-a0773.appspot.com/o/images%2F1d.jpg?alt=media&token=0a9195b4-a830-43a7-a511-4956284c7dbe",
+    heading: "Woman Fashion",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+];
